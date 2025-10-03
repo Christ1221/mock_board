b78d6c2d3739f7d70bbb3a97072403cb185fca14
cordy <mglgain@gmail.com>
2025-10-03 18:29:32 +0800

    initial

--- src/app/components/index.jsx
+++
@@ -1,6 +0,0 @@
-
-export * from "./landpage";
-
-export { default as Log_In } from "./login";
-
-export { default as Sign_Up } from "./signup";
--- src/app/components/landpage/index.jsx
+++
@@ -1,6 +0,0 @@
-
-export { default as Header } from "./Header";
-
-export { default as Banner } from "./Banner";
-
-export { default as Footer } from "./Footer";
